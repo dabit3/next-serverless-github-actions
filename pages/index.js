3e492005b9fb9dc5e682c456dc6e2b5c749ec07f
Nader Dabit <dabit3@gmail.com>
2020-12-14 18:27:19 -0600

    finalize commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Hello from second live update!</a>
+          Welcome to <a href="https://nextjs.org">THIS ISN'T EVEN MY FINAL FORM</a>
         </h1>
       </main>
 
